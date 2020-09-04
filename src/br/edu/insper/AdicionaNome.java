f88137b6ff87683b63df2eb5d2929a630c88fb5a
LaisNSilva <silva.lais72@hotmial.com>
2020-09-03 21:51:35 -0300

    add email e curso

--- src/br/edu/insper/AdicionaNome.java
+++ src/br/edu/insper/AdicionaNome.java
@@ -42,10 +42,25 @@ public class AdicionaNome extends HttpServlet {
 	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		String nome = request.getParameter("nome");
+		String email = request.getParameter("email");
+		String curso = request.getParameter("curso");
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
 		out.println("<body>");
 		out.println("Nome: " + nome);
+		
+		out.println("</body>");
+		out.println("</html>");
+		
+		out.println("<html>");
+		out.println("<body>");
+		out.println("E-mail: " + email);
+		out.println("</body>");
+		out.println("</html>");
+		
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Curso: " + curso);
 		out.println("</body>");
 		out.println("</html>");
 	}
